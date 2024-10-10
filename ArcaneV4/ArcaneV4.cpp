2dd16a5861912913fe81e4409273899e0ee12a97
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-10-10 01:14:17 +0100

    http server :fire:

--- ArcaneV4/ArcaneV4.cpp
+++ ArcaneV4/ArcaneV4.cpp
@@ -2,8 +2,23 @@
 #include "crow.h"
 #include <boost/asio.hpp>
 
+int startHTTPServer(int port);
+
 int main()
 {
     std::cout << "ArcaneV4 Is Starting!\n";
+    startHTTPServer(3551);
+    return 0;
+}
+
+int startHTTPServer(int port) {
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([]() {
+        return "ArcaneBackendV4";
+    });
+
+    std::cout << "ArcaneV4 Running On Port: " << port << "\n";
+    app.port(port).multithreaded().run();
     return 0;
 }

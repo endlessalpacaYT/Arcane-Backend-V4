29982093108aa5a9c33cad85864bfe97f66f778a
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-10-10 01:34:04 +0100

    async

--- ArcaneV4/ArcaneV4.cpp
+++ ArcaneV4/ArcaneV4.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
+#include <thread>
+#include <future>
 #include "crow.h"
 #include <boost/asio.hpp>
 
-int startHTTPServer(int port);
+void startHTTPServer(int port);
 
 int main()
 {
-    std::cout << "ArcaneV4 Is Starting!\n";
-    startHTTPServer(3551);
-    return 0;
+    int port = 3551;
+    std::cout << "ArcaneV4 Is Starting on Port: " << port << "\n";
+
+    auto httpFuture = std::async(std::launch::async, startHTTPServer, port);
+
+    return 0;  
 }
 
-int startHTTPServer(int port) {
+void startHTTPServer(int port) {
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")([]() {
         return "ArcaneBackendV4";
     });
 
-    std::cout << "ArcaneV4 Running On Port: " << port << "\n";
     app.port(port).multithreaded().run();
-    return 0;
 }

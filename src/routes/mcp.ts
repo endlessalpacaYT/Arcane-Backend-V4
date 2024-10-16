46a94bc7ded7a7c0de21db24d73abac02fc99ad8
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-10-16 22:07:57 +0100

    redoing mcp

--- src/routes/mcp.ts
+++ src/routes/mcp.ts
@@ -10,160 +10,17 @@ const common_public = require("../responses/DefaultProfiles/common_public.json")
 const common_core = require("../responses/DefaultProfiles/common_core.json")
 
 export async function mcpRoutes(fastify: FastifyInstance) {
-    interface AccountParams {
+    interface operationParams {
         accountId: string;
-    }
-
-    interface QueryProfile {
-        profileId: string;
-        rvn: String;
+        operation: string;
     }
     
-    fastify.post('/fortnite/api/game/v2/profile/:accountId/client/QueryProfile', async (request: FastifyRequest<{ Params: AccountParams, Querystring: QueryProfile }>, reply: FastifyReply) => {
+    fastify.post('/fortnite/api/game/v2/profile/:accountID/client/:operation', async (request: FastifyRequest<{ Params: operationParams }>, reply: FastifyReply) => {
         try {
-            const { accountId } = request.params;
-            let { rvn } = request.query;
-
-            let profile = await Profiles.findOne({ accountId: accountId });
-            if (!profile) {
-                profile = await profileman.createProfile(accountId);
-            }
-
-            const rvnNumber = Number(rvn);
-            
-            if (request.query.profileId == "common_public") {
-                profileman.updateProfileRvn(rvnNumber, "common_public", accountId)
-                return reply.status(200).send(profile.profiles.common_public);
-            } else if (request.query.profileId == "common_core") {
-                profileman.updateProfileRvn(rvnNumber, "common_core", accountId)
-                return reply.status(200).send(profile.profiles.common_core);
-            } else if (request.query.profileId == "athena") {
-                profileman.updateProfileRvn(rvnNumber, "athena", accountId)
-                return reply.status(200).send(profile.profiles.athena);
-            } else if (request.query.profileId == "creative") {
-                profileman.updateProfileRvn(rvnNumber, "creative", accountId)
-                return reply.status(200).send(profile.profiles.athena);
-            } else {
-                console.log(request.query.profileId);
-                return reply.status(200).send({
-                    status: "OK",
-                    code: 200
-                })
-            }
-        } catch (err) {
-            console.error(err);
-            return reply.status(500).send({
-                error: "SERVER ERROR"
+            return reply.status(200).send({
+                status: "OK",
+                code: 200
             })
-        }
-    })
-
-    fastify.post('/fortnite/api/game/v2/profile/:accountId/client/SetMtxPlatform', async (request: FastifyRequest<{ Params: AccountParams, Querystring: QueryProfile }>, reply: FastifyReply) => {
-        try {
-            const { accountId } = request.params;
-            let { rvn } = request.query;
-
-            let profile = await Profiles.findOne({ accountId: accountId });
-            if (!profile) {
-                profile = await profileman.createProfile(accountId);
-            }
-
-            const rvnNumber = Number(rvn);
-            
-            if (request.query.profileId == "common_public") {
-                profileman.updateProfileRvn(rvnNumber, "common_public", accountId)
-                return reply.status(200).send(profile.profiles.common_public);
-            } else if (request.query.profileId == "common_core") {
-                profileman.updateProfileRvn(rvnNumber, "common_core", accountId)
-                return reply.status(200).send(profile.profiles.common_core);
-            } else if (request.query.profileId == "athena") {
-                profileman.updateProfileRvn(rvnNumber, "athena", accountId)
-                return reply.status(200).send(profile.profiles.athena);
-            } else if (request.query.profileId == "creative") {
-                profileman.updateProfileRvn(rvnNumber, "creative", accountId)
-                return reply.status(200).send(profile.profiles.athena);
-            } else {
-                return reply.status(200).send({
-                    status: "OK",
-                    code: 200
-                })
-            }
-        } catch (err) {
-            console.error(err);
-            return reply.status(500).send({
-                error: "SERVER ERROR"
-            })
-        }
-    })
-
-    fastify.post('/fortnite/api/game/v2/profile/:accountId/client/ClaimMfaEnabled', async (request: FastifyRequest<{ Params: AccountParams, Querystring: QueryProfile }>, reply: FastifyReply) => {
-        try {
-            const { accountId } = request.params;
-            let { rvn } = request.query;
-
-            let profile = await Profiles.findOne({ accountId: accountId });
-            if (!profile) {
-                profile = await profileman.createProfile(accountId);
-            }
-
-            const rvnNumber = Number(rvn);
-            
-            if (request.query.profileId == "common_public") {
-                profileman.updateProfileRvn(rvnNumber, "common_public", accountId)
-                return reply.status(200).send(profile.profiles.common_public);
-            } else if (request.query.profileId == "common_core") {
-                profileman.updateProfileRvn(rvnNumber, "common_core", accountId)
-                return reply.status(200).send(profile.profiles.common_core);
-            } else if (request.query.profileId == "athena") {
-                profileman.updateProfileRvn(rvnNumber, "athena", accountId)
-                return reply.status(200).send(profile.profiles.athena);
-            } else if (request.query.profileId == "creative") {
-                profileman.updateProfileRvn(rvnNumber, "creative", accountId)
-                return reply.status(200).send(profile.profiles.athena);
-            } else {
-                return reply.status(200).send({
-                    status: "OK",
-                    code: 200
-                })
-            }
-        } catch (err) {
-            console.error(err);
-            return reply.status(500).send({
-                error: "SERVER ERROR"
-            })
-        }
-    })
-
-    fastify.post('/fortnite/api/game/v2/profile/:accountId/client/ClientQuestLogin', async (request: FastifyRequest<{ Params: AccountParams, Querystring: QueryProfile }>, reply: FastifyReply) => {
-        try {
-            const { accountId } = request.params;
-            let { rvn } = request.query;
-
-            let profile = await Profiles.findOne({ accountId: accountId });
-            if (!profile) {
-                profile = await profileman.createProfile(accountId);
-            }
-
-            const rvnNumber = Number(rvn);
-            
-            if (request.query.profileId == "common_public") {
-                profileman.updateProfileRvn(rvnNumber, "common_public", accountId)
-                return reply.status(200).send(profile.profiles.common_public);
-            } else if (request.query.profileId == "common_core") {
-                profileman.updateProfileRvn(rvnNumber, "common_core", accountId)
-                return reply.status(200).send(profile.profiles.common_core);
-            } else if (request.query.profileId == "athena") {
-                profileman.updateProfileRvn(rvnNumber, "athena", accountId)
-                return reply.status(200).send(profile.profiles.athena);
-            } else if (request.query.profileId == "creative") {
-                profileman.updateProfileRvn(rvnNumber, "creative", accountId)
-                return reply.status(200).send(profile.profiles.athena);
-            } else {
-                return reply.status(200).send({
-                    status: "OK",
-                    code: 200
-                })
-            }
         } catch (err) {
             console.error(err);
             return reply.status(500).send({
